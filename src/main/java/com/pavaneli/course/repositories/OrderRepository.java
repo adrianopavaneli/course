42333e91ca919141bcc690d7e591f38ec8147a9d
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-03-18 22:07:43 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/pavaneli/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.pavaneli.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pavaneli.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+
+}
