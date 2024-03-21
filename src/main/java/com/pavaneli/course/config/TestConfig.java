b96f87a9c3966e9cfb31a135969ab3581a760cad
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-03-21 20:48:43 -0300

    pAYMENT, ONE-TO-ONE ASSOCIATION

--- src/main/java/com/pavaneli/course/config/TestConfig.java
+++ src/main/java/com/pavaneli/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pavaneli.course.entities.Category;
 import com.pavaneli.course.entities.Order;
 import com.pavaneli.course.entities.OrderItem;
+import com.pavaneli.course.entities.Payment;
 import com.pavaneli.course.entities.Product;
 import com.pavaneli.course.entities.User;
 import com.pavaneli.course.entities.enums.OrderStatus;
@@ -71,6 +72,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAllAndFlush(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);		
 	}
 	
 	
